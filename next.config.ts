6c0d1d3016d63932fa3af6434fd705551cc4ef4d
crazyailife03 <crazyailife03@gmail.com>
2026-05-19 17:43:32 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: process.env.NODE_ENV === "production" ? "/團購平台" : "",
+  basePath: process.env.NODE_ENV === "production" ? "/demo/group-buy" : "",
   images: {
     unoptimized: true,
     remotePatterns: [
